498fbd1451ab013396ec8b4590bd3d4db38f49f4
Arman Maurya <mauryaarman5@gmail.com>
2026-06-28 22:26:42 +0530

    chore: bump to version 0.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.armanmaurya.internetradio"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.6.4"
+        versionCode = 8
+        versionName = "0.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
